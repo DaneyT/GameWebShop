f0fbc907a5504edf568049202b3c66c03a0a5687
Dan <daneytimmers@hotmail.com>
2016-09-15 00:04:20 +0200

    first commit

---
+++ GameWebShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GameWebShop.Startup))]
+namespace GameWebShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ GameWebShop/ViewModels/RandomGameViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GameWebShop.Models;
+
+namespace GameWebShop.ViewModels
+{
+    public class RandomGameViewModel
+    {
+        public Game Game { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
