77256e6e5a089699d576effa2f3ddbb38c4b0176
Dan <daneytimmers@hotmail.com>
2016-09-22 23:51:20 +0200

    Added autorisation and login without DB

--- GameWebShop/Controllers/AuthController.cs
+++ GameWebShop/Controllers/AuthController.cs
@@ -1,18 +1,40 @@
-﻿using System;
+﻿using GameWebShop.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace GameWebShop.Controllers
 {
     public class AuthController : Controller
     {
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToRoute("home");
+        }
         public ActionResult Login()
         {
 
+            return View(new AuthLogin
+                {
+                });
+        }
 
-            return View();
+        [HttpPost]
+        public ActionResult Login(AuthLogin form, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+                return View(form);
+
+            FormsAuthentication.SetAuthCookie(form.Username, true);
+
+            if(!string.IsNullOrWhiteSpace(returnUrl))
+                return Redirect(returnUrl);
+
+            return RedirectToRoute("home");
         }
     }
 }
